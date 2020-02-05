4176d6528028c2f499c5b5d6db3942d812c9f986
Curtis Chou <47016201+choucurtis987@users.noreply.github.com>
2020-02-04 21:39:05 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Scrappy KMeans Project
+- [trying_kmeans.py (FINAL VERSION)](https://github.com/choucurtis987/custom_projects/blob/master/kmeans_stuff/trying_kmeans.py) 
+- [basic use of kmeans algorithm](https://github.com/choucurtis987/custom_projects/blob/master/kmeans_stuff/basic_use_of_kmeans.py)
+
+## My K-Means Algorithm Built From Scratch
+- The algorithm ultilizes a variety of Python data structures and OOP to group data together
+- Ultilizes Python Libraries such as Numpy and Matplotlib
+- Groups a set of datapoints together based on K amount of groups
+- Returns a Python dictionary where the key is group number and value is an array of array points
+
+## Intro to KMeans Clustering (How it works)
+- KMeans is an unsupervised machine learning algorithm. 
+- K represents the number of clusters/centroids the dataset has. 
+- Centroids are simply just the mean point or center of a group of points
+- The algorithm begins by selecting K amount of random centroids where it then finds the eudlidean distance between the data points and K number of centroids. The points closest to a certain centroid will be classified as a cluster with that centroid. The algorithm will then find the mean of all the points in that cluster and the mean will be the new centroid. It will continuously do this until the mean of all clusters stop changing and are optimized.  
+
+## Purpose of KMeans
+- "group similar data points together and discover underlying patterns" as stated by AndreyBu 
+- ex. market segmentation and fraud detection 
+
+## Abstract
+- a visualization of what the algorithm does
+<img src="/images/readme1.png" width="1000" height="400">
+<img src="/images/readme2.png" width="1000" height="400">
+<img src="/images/readme3.png" width="1000" height="400">
+
+## Future Directions
+- Build a more accurate model that classfies and groups data more accurately
+- Implement recursion?
---
+++ trying_kmeans.py
@@ -0,0 +1,117 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+class scrap_kmeans:
+
+    def __init__(self, k=2):
+        self.k = k
+
+    # takes numpy array or list of lists as argument:
+    def fit(self,data):
+        centroids = set()
+
+        # finding the first random centroids:
+        while self.k:
+            num = np.random.randint(0,len(data))
+            centroid = tuple(data[num])
+            centroids.add(centroid)
+            if len(centroids) == self.k:
+                break
+
+
+        points = [np.array(point) for point in data]
+
+        # starting centroids:
+        first_centroids = [np.array(centroid) for centroid in centroids]
+
+
+        # classifying the points:
+        # centroids and points take a list of arrays as argument:
+        def classify(centroids, points):
+            num = 0
+            blank = []
+            classification = {}
+
+            for i in range(self.k):
+                classification[i] = []
+
+            while True:
+                for centroid in centroids:
+
+                    euclidean_distance = float( np.linalg.norm(centroid - points[num]) )
+                    # adding distances to a list to find the min:
+                    blank.append(euclidean_distance)
+
+                classification[ blank.index(min(blank))].append(list(points[num]))
+                num += 1
+                blank = []
+                if num == len(points):
+                    break
+
+            return classification
+
+        # takes dict as argument:
+        def update(classification):
+            # new centroids
+            blank = []
+            # loop through dict
+            for i in classification:
+                classification[i] = np.array(classification[i])
+                # find the mean of xs and ys giving a new centroid
+                new_centroids_x = np.mean( classification[i][:,0] )
+                new_centroids_y = np.mean( classification[i][:,1] )
+                new_centroids = np.array([new_centroids_x,new_centroids_y])
+
+                # add that centroid to a list
+                blank.append(new_centroids)
+
+            return blank
+
+        # first classification of points based on random centroids:
+        classification = classify(first_centroids, points)
+
+        # storage of all centroids:
+        storage = []
+
+        # this while statement optimizes the centroid to the point where a new centroid no longer changes the
+        # classification of the points:
+        while True:
+
+            if not storage:
+                centroids = update(classification)
+                new_classification = classify(centroids, points)
+                storage.append(centroids)
+
+            test = classify(storage[-1], points)
+
+            if storage:
+                centroids = update(test)
+
+                new_classification = classify(centroids, points)
+                storage.append(centroids)
+                for i in new_classification:
+                    new_classification[i] = np.array(new_classification[i])
+
+                a = new_classification[i] == test[i]
+
+            # np.all is a conditional to compare numpy arrays
+            if np.all(a):
+                break
+
+        # this finds the true centroid or reassures it
+        blank = []
+        for i in new_classification:
+            new_x = new_classification[i][:,0].mean()
+            new_y = new_classification[i][:,1].mean()
+            new_centroid = [new_x,new_y]
+            blank.append(new_centroid)
+        storage.append(blank)
+
+        # process of changing centroids, uncomment to see
+        # for num, i in enumerate(storage):
+        #     print(f'{num}: {i}')
+
+        self.centroids = storage[-1]
+
+        return new_classification
